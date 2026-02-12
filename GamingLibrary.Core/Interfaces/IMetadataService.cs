1ef83343e258ee003c9ca82c24c486788961f2bd
unknown <jm.adams32100@gmail.com>
2026-02-12 12:47:48 -0700

    Added Steam API connection and IGDB API for metadata lookup

---
+++ GamingLibrary.Core/Interfaces/IMetadataService.cs
@@ -0,0 +1,12 @@
+﻿using GamingLibrary.Core.DTOs.IGDB;
+using GamingLibrary.Core.Entities;
+
+namespace GamingLibrary.Core.Interfaces
+{
+    public interface IMetadataService
+    {
+        Task<Game> EnrichGameMetadataAsync(Game game);
+
+        Task<IgdbGame?> SearchGameAsync(string title);
+    }
+}
